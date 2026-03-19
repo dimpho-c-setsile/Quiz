0c3d426558c5310fab13ba1fff389c6100f30629
Dimpho Setsile <vucharity@gmail.com>
2026-03-19 04:38:01 +0200

    Added data structure for questions

---
+++ quiz-backend/Models/Question.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Quiz_app.Models
+{
+    public class Question
+    {
+        [Key]
+        public int QnId { get; set;  }
+        public required string QuestionInwords { get; set; }
+        public string? pictureNmae { get; set; }
+        public required string  Option1 { get; set; }
+        public required string Option2 { get; set; }
+        public required string Option3 { get; set; }
+        public required string Option4 { get; set; }
+        public required string Answer { get; set; }
+
+    }
+}
